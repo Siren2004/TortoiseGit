91ba378f87222e0621b919ba2ed3cca71799478c
Сергей <s.kotlyarchuk@mail.ru>
2024-10-22 01:19:23 +0300

    Добавлен ввод двух целых чисел в main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+   #include "sdt.h"
+   #include <iostream>
+
+   int main() {
+       setlocale(LC_ALL, "");
+       int num1, num2;
+
+       std::cout << "¬ведите первое целое число: ";
+       std::cin >> num1;
+       std::cout << "¬ведите второе целое число: ";
+       std::cin >> num2;
+
+       return 0;
+   }
